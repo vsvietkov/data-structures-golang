ee0e69a85fc2fe1395dc7bd915c9136ee23f43cd
Vitalii Svietkov <vitalii.svietkov@weblabs.pro>
2024-06-05 01:35:55 +0300

    Added memory output to benchmarks and updated stack benchmark

--- benchmarks/stack/stack_test.go
+++ benchmarks/stack/stack_test.go
@@ -10,6 +10,25 @@ func BenchmarkPush(b *testing.B) {
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
+		if err := s.Push(i); err != nil {
+			b.Error(err)
+		}
+	}
+}
+
+func BenchmarkPop(b *testing.B) {
+	s, _ := stack.NewStack(b.N)
+
+	for i := 0; i < b.N; i++ {
 		s.Push(i)
+	}
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		_, err := s.Pop()
+
+		if err != nil {
+			b.Error(err)
+		}
 	}
 }
